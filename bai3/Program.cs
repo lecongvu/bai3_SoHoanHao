9c62203c804f875ff2830c5498c7b2ab1a947fd7
Le Cong Vu <Le Cong Vu@MSI>
2022-11-15 23:47:00 +0700

    update

---
+++ bai3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace bai3
+{
+    class Program
+    {
+        static bool kiemTra(int A)
+        {
+            int sum = 0;
+            for (int i = 1; i <= (A / 2); i++)
+            {
+                if (A % i == 0)
+                {
+                    sum += i;
+                    if (sum == A)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+            //chữ có daasu
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhap so ");
+            int A = int.Parse(Console.ReadLine());
+            if (kiemTra(A))
+            {
+                Console.WriteLine("Là số hoàn hảo");
+            }
+            else
+            {
+                Console.WriteLine("không phải số hoàn hảo");
+            }
+            Console.ReadLine();
+        }
+    }
+}
